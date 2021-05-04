701d3d71f3652a1b8f39e1aa0058302e527913f1
Habiba1215 <75575107+Habiba1215@users.noreply.github.com>
2021-05-04 14:25:29 +0300

    Add files via upload

---
+++ synchronousBallMovement-master/synchronousBallMovement-master/sketch.js
@@ -0,0 +1,19 @@
+var hypnoticBall, database;
+var position;
+var playerCount, form, player, game
+var gameState = 0
+
+function setup(){
+  database = firebase.database();
+  createCanvas(400,400);
+
+  game = new Game() 
+  game.getState()
+  game.start()
+}
+
+function draw(){
+  
+  
+}
+
